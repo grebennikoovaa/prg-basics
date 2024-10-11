58ad3d6f239cc176b14b22c6139ccdcd376d0216
Anastasiia Hrebennikova <grebennikova.anastasya@gmail.com>
2024-10-11 09:56:06 +0200

    section 3

---
+++ 02-TypesAndVariables/2.2.py
@@ -0,0 +1,10 @@
+###
+# A program that calculates the sum of two numbers.
+# Modify the program to calculate the sum of three numbers.
+#
+number1 = 45
+number2 = 89
+result = number1 + number2
+print('Number 1: ', number1)
+print('Number 2: ', number2)
+print('The result of summation: ', result)
---
+++ 02-TypesAndVariables/2.6.py
@@ -0,0 +1,5 @@
+import math
+height = input('Height: ')
+height = float(height)
+distance = 3.57 * math.sqrt(height)
+print('The distance is: ', distance)
