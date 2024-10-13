0b7021ddcda5b5aa06d78045916f930780678ca3
grebennikoovaa <237433@student.uek.krakow.pl>
2024-10-13 11:01:40 +0200

    Delete 02-TypesAndVariables/1.4.py

--- 02-TypesAndVariables/1.4.py
+++
@@ -1,8 +0,0 @@
-###
-# The program simulates five dice rolls.
-#
-import random
-print("Dice rolling simulator")
-for i in range(5):
-    dice_roll = random.randint(1,6)
-    print(dice_roll, end=" ")
