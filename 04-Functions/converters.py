da2343fbfcf1c2234d19d6d3dd8b7789a86e3ae4
Anastasiia Hrebennikova <grebennikova.anastasya@gmail.com>
2024-11-05 21:15:16 +0100

    Lesson 4

--- 04-Functions/converters.py
+++ 04-Functions/converters.py
@@ -9,8 +9,8 @@ def inch_to_cm(n):
     return n*2.54
 
 if __name__ == "__main__":
-    #  only execute when you run this module
-    # so you can test the functions in this place
+    # only execute when you run this module
+    # so you can test the functions in this place.
     print(f'20cm = {cm_to_inch(2)}cm')
     print(f'5 feet 6 inches = {feet_to_inch(5,6)}m')
     print(f'20inches = {inch_to_cm(20)}cm')
