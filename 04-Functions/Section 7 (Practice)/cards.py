268257812d0b53cc4e0018e58462fb1d5a2e3b88
Anastasiia Hrebennikova <grebennikova.anastasya@gmail.com>
2024-11-13 20:49:24 +0100

    Lesson 4

--- 04-Functions/Section 7 (Practice)/cards.py
+++ 04-Functions/Section 7 (Practice)/cards.py
@@ -1,5 +1,5 @@
 def hide(card_number):
     first_let = card_number[0:2]
     last_let = card_number[12:16]
-    hiden = f"{first_let}**********{last_let}"
-    return hiden
+    hidden = f"{first_let}**********{last_let}"
+    return hidden
