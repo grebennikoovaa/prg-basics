ed14bb96f80682ef46bb859d1c01cdd671d13466
Anastasiia Hrebennikova <grebennikova.anastasya@gmail.com>
2024-11-14 14:41:26 +0100

    Lesson 4

--- 04-Functions/Section 7 (Practice)/7.28.py
+++ 04-Functions/Section 7 (Practice)/7.28.py
@@ -1,5 +1,3 @@
-n = int(input('Enter a number: '))
-
 def factorial(n):
     # 0! = 1, 1! = 1
     if n == 0 or n == 1:
@@ -8,7 +6,8 @@ def factorial(n):
     # n! = n * (n-1)!
     if n > 1:
         return n * factorial(n-1)
+    return factorial()
 
-# Calculate factorial of 5
-result = factorial(n)
-print(f"The factorial of {n} is: {result}")
+
+if __name__ == "__main__":
+    print(factorial(5))
