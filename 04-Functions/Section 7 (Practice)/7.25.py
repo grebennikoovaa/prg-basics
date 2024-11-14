a64ab616c6f610be5c40fc0e24f58fbf863e5a22
Anastasiia Hrebennikova <grebennikova.anastasya@gmail.com>
2024-11-14 13:43:24 +0100

    Lesson 4

---
+++ 04-Functions/Section 7 (Practice)/7.25.py
@@ -0,0 +1,11 @@
+def f(text):
+    result = ""
+    for char in text:
+        result = result + char + '-'
+    if result:
+        result = result[:-1]
+    return result
+
+if __name__ == "__main__":
+    print(f("University"))
+    print(f("UE"))
